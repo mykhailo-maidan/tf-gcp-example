c4e8a084d60a3ceafcf5368b5c0b57de3a908b76
mykhailo-maidan <mykhailo.maidan@gmail.com>
2024-05-07 13:16:19 +0200

    Add cloud storage for terraform

--- main.tf
+++ main.tf
@@ -50,3 +50,9 @@ resource "local_file" "kubeconfig" {
   file_permission = "0400"
 }
 
+terraform {
+  backend "gcs" {
+    bucket = "terraform-state-mykhailo"
+    prefix = "terraform/state"
+  }
+}
